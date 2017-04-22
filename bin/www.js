caf25dccbdb87014ef74de1327d1c8ea87f441d7
Alexander <a.yerokhovets@outlook.com>
2017-04-22 22:13:22 +0300

    Initial commit

--- bin/www.js
+++ bin/www.js
@@ -70,7 +70,7 @@ function onError(error) {
 
 function onListening() {
     soap.listen(app, '/wsdl', service.myService, service.xml)
-    let addr = server.address();
+    const addr = server.address();
     const bind = typeof addr === 'string'
         ? 'pipe ' + addr
         : 'port ' + addr.port;
