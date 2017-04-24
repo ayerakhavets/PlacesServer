bf208f5c18739274912a6d1b97d9b6433532bcbf
Alexander <a.yerokhovets@outlook.com>
2017-04-24 17:52:19 +0300

    WorkTime model restructured

--- models/workTime.js
+++ models/workTime.js
@@ -1,9 +1,23 @@
 import mongoose from 'mongoose';
-import time     from './time';
+
+const TimeSchema = new mongoose.Schema({
+    hours: {
+        type: Number,
+        min: [0, 'wrong format'],
+        max: [23, 'wrong format']
+    },
+    minutes: {
+        type: Number,
+        min: [0, 'wrong format'],
+        max: [59, 'wrong format']
+    }
+});
+//mongoose.model('Time', TimeSchema);
+
 
 const WorkTimeSchema = new mongoose.Schema({
-    open: time.schema,
-    close: time.schema
+    open: TimeSchema,
+    close: TimeSchema
 }, {_id: false});
 
 export default mongoose.model('WorkTime', WorkTimeSchema)
